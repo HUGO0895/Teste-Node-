05851b6b16f105ad3ed84184b8a440b9506c4a55
Hugo Leo <hugoleonardo040906@gmail.com>
2026-02-22 18:42:48 -0300

    aa

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const route=express.Router();
 const homeControler=require('./src/controllers/homecontroler.js')
 const contatoControler=require('./src/controllers/contatoControler.js')
 
-route.get('/',(req,res,next)=>{console.log('09'); next()},homeControler.home); // essa arrow function é um midleware
+route.get('/',homeControler.home); // essa arrow function é um midleware
 route.post('/',homeControler.homepost);
 route.get('/contato',contatoControler.contato)
 
--- server.js
+++ server.js
@@ -1,10 +1,35 @@
+require('dotenv').config();
 const express =require('express');
 const path=require('path');
 const app=express();
-const routes=require('./routes.js')
-app.use(express.urlencoded({extended:true})) // pega as req por formularios
+const mongoose=require('mongoose');
+mongoose.connect(process.env.CONECTION_MONGO)
+    .then(() => {
+        console.log('conectado aqui');
+      app.emit('conectado!');
+    }).catch(e=>{console.log(e)})
 
-app.use(express.static(path.resolve(__dirname,'public')))
+const session=require('express-session');
+const MongoStore=require('connect-mongo');
+const flash =require('connect-flash');
+
+const routes=require('./routes.js')
+app.use(express.urlencoded({extended:true})); // pega as req por formularios
+
+const sessionOptions={
+    secret: process.env.sessionSenha,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ 
+        mongoUrl: process.env.CONECTION_MONGO, 
+        collectionName: 'sessions'
+    }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 *7
+    }
+};
+app.use(session(sessionOptions));
+app.use(flash());
 
 app.set('views',path.resolve(__dirname,'src','views'));
 
@@ -12,7 +37,9 @@ app.set('view engine','ejs');
 
 app.use(routes);
 
+app.on('conectado!',()=>{
 app.listen(3000,()=>{
     console.log("Acessar http://localhost:3000")
     console.log("Servidor executando na porta 3000")
+});
 });
